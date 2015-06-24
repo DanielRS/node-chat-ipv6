6876e69d83d9da1f87b9e2f6a411d4d7b5b4fe0b
Daniel Rivas <ers.daniel+dev@gmail.com>
2015-06-24 15:26:08 -0700

    Fixed server listening address

--- tests/server.js
+++ tests/server.js
@@ -2,8 +2,12 @@ var
 	Server = require('../lib/Agents/Server'),
 	addrs = require('../lib/Misc/addresses');
 
-var server = new Server(addrs.channels.everyone);
+var server = new Server(addrs.channels.server);
 
 server.on('listening', function() {
 	console.log('Server listening for connections...');
+});
+
+server.on('message', function() {
+	console.log('Server received a message...');
 });
