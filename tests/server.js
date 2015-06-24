ff993cbfbc1d0b23ee5e1b319ef67bad36a0714a
Daniel Rivas <ers.daniel+dev@gmail.com>
2015-06-24 16:14:59 -0700

    Changed server channel from everyone to server only

--- tests/server.js
+++ tests/server.js
@@ -2,12 +2,8 @@ var
 	Server = require('../lib/Agents/Server'),
 	addrs = require('../lib/Misc/addresses');
 
-var server = new Server(addrs.channels.everyone);
+var server = new Server(addrs.channels.server);
 
 server.on('listening', function() {
 	console.log('Server listening for connections...');
-});
-
-server.on('message', function(message, remote) {
-	console.log(JSON.parse(message));
 });
